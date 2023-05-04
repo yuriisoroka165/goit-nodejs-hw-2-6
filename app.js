9c5371692e4e18776732633a820e03ded56de25c
Yurii Soroka <yurisoroka165@gmail.com>
2023-05-04 14:15:29 +0300

    create routes and HttpError helper

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+// перевіряє тіло запиту на content type application/json і повертає обєкт
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
+app.use("/api/contacts", contactsRouter);
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+app.use((request, response) => {
+    response.status(404).json({ message: "Not found" });
+});
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+// функція з 4 параметрами це функція обробник помилок (де перший параметр це помилка)
+app.use((error, request, response, next) => {
+    const { status = 500, message = "Server error" } = error;
+    response.status(status).json({ message });
+});
 
-module.exports = app
+module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,86 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const Joi = require("joi");
 
-const router = express.Router()
+const contacts = require("../../models/contacts");
+const { HttpError } = require("../../helpers");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// Joi схема це опис вимог до обєкта
+const addSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    // номер телефону повинен бути у форматі (111) 111-1111
+    phone: Joi.string().pattern(/^\(\d{3}\)\s\d{3}-\d{4}$/),
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (request, response, next) => {
+    try {
+        const result = await contacts.listContacts();
+        if (!result) {
+            throw HttpError(404, "Not found");
+        }
+        response.json(result);
+    } catch (error) {
+        next(error);
+    }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (request, response, next) => {
+    try {
+        const { contactId } = request.params;
+        const result = await contacts.getContactById(contactId);
+        if (!result) {
+            throw HttpError(404, "Not found");
+        }
+        response.json(result);
+    } catch (error) {
+        next(error);
+    }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (request, response, next) => {
+    try {
+        // перед додаванням дані проходять валідацію
+        const { error } = addSchema.validate(request.body);
+        // якщо є помилка валідації відправляємо її
+        if (error) {
+            throw HttpError(400, error.message);
+        }
+        const result = await contacts.addContact(request.body);
+        response.status(201).json(result);
+    } catch (error) {
+        next(error);
+    }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (request, response, next) => {
+    try {
+        const { contactId } = request.params;
+        const result = await contacts.removeContact(contactId);
+        if (!result) {
+            throw HttpError(404, "Not found");
+        }
+        response.json({ message: "contact deleted" });
+    } catch (error) {
+        next(error);
+    }
+});
 
-module.exports = router
+router.put("/:contactId", async (request, response, next) => {
+    try {
+        const { error } = addSchema.validate(request.body);
+        if (error) {
+            throw HttpError(400, error.message);
+        }
+        const { contactId } = request.params;
+        const result = await contacts.updateContact(contactId, request.body);
+        if (!result) {
+            throw HttpError(404, "Not found");
+        }
+        response.json(result);
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
